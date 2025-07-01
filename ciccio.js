ac35582252cd08d56b0af47d174f254f3a46dda5
francesco mansi <f.mansi@socialwebsolutions.it>
2025-07-01 17:45:47 +0200

    Aggiunta file

---
+++ ciccio.js
@@ -0,0 +1,63 @@
+// let nome = 'Francesco         [205678'; //stringa
+// let telefono = 892736738; // intero
+// let price = 67.89; //float
+// let status = true;// booleano
+// let vuoto = null;//nullo 
+// let nondefinita = undefined;//nullo 
+
+// console.log(typeof nome);
+// console.log(typeof price);
+
+// operatori matematici
+//somma, sottrazione, divisione e moltiplicazioni
+
+// let num1 = 10;
+// let num2 = 6;
+// console.log(num1);
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+
+// num2 = num2 + 1;//somma
+// num2++;//somma
+// num1 = num1 - 1;//sottrazione
+// num1--;//sottrazione
+// console.log(num1);
+// num1 = num1 + 6;
+// num1 += 6;
+// Operatori di confronti
+
+
+// let a = 11;
+// let b = '11';
+//risultato sarà sempre una condizione: vera o falsa
+// console.log(5 > 8); //Maggiore
+// console.log(a >= 5);//Maggiore uguale
+// console.log(b <= 5);//Minore uguale
+// console.log(b < a);//Minore
+// console.log(b == a);//Uguaglianza 
+// console.log(b === a);//Uguaglianza stretta
+// console.log(b != a);//Diverso
+// console.log(b !== a);//Diverso stretto
+
+//Operatori logici
+
+let stato1 = true;
+let stato2 = false;
+
+//AND
+console.log(true && stato2);//false
+console.log(stato1 && stato2);//false
+//console.log(stato1 && !stato2);//true
+
+//OR  
+console.log(stato1 || stato2);//true
+console.log(stato2 || false);//false
+
+//NOT
+console.log(!stato1);//false
+
+console.log(stato1 || !(false && stato2));
+
--- script2.js
+++ script2.js
@@ -7,4 +7,5 @@ firstname = "Francesco";//Assegnazione
 let lastName = "mansi";
 
 const PI_GRECO = 3.14;
+
 console.log('Ciao, mi chiamo ' + firstname + ' il mio congome è ' + lastName);
