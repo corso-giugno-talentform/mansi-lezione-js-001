3d9d28454cd1f2735e3af006591e8f891276280e
francesco mansi <f.mansi@socialwebsolutions.it>
2025-07-01 12:32:12 +0200

    Aggiunta file

---
+++ script.js
@@ -0,0 +1,2 @@
+//alert('CIAO sono dentro script!');
+console.log('Sono un messaggio della console.');
---
+++ script2.js
@@ -0,0 +1,10 @@
+
+var nome; //No
+
+let firstname; // dichiarazione
+firstname = "Francesco";//Assegnazione
+
+let lastName = "mansi";
+
+const PI_GRECO = 3.14;
+console.log('Ciao, mi chiamo ' + firstname + ' il mio congome è ' + lastName);
